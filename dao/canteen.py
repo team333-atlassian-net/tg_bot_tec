b8b356e85d838c4bcbe6981653ccbd297bb42f9a
vlkttlv <vitavitavita04@bk.ru>
2025-06-11 17:41:38 +0700

    добавлен dao класс для работы с БД

---
+++ dao/canteen.py
@@ -0,0 +1,10 @@
+from sqlalchemy.future import select
+from models import CanteenInfo
+from db import async_session_maker
+
+async def get_latest_canteen_info() -> CanteenInfo | None:
+    async with async_session_maker() as session:
+        result = await session.execute(
+            select(CanteenInfo).order_by(CanteenInfo.created_at.desc()).limit(1)
+        )
+        return result.scalar_one_or_none()
