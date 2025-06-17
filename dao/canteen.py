09ad864b52cc922434baaf7e2c52be84bd7f8bc3
vlkttlv <vitavitavita04@bk.ru>
2025-06-17 22:43:10 +0700

    комментарии

--- dao/canteen.py
+++ dao/canteen.py
@@ -3,6 +3,7 @@ from models import CanteenInfo
 from db import async_session_maker
 
 async def get_latest_canteen_info() -> CanteenInfo | None:
+    """Получение информации о столовой"""
     async with async_session_maker() as session:
         result = await session.execute(
             select(CanteenInfo).order_by(CanteenInfo.created_at.desc()).limit(1)
