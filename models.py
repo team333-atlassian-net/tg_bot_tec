5777a068bf30f0e6d9f386b4140a9da9a454d576
vlkttlv <vitavitavita04@bk.ru>
2025-06-17 19:40:25 +0700

    убрала ограничение в модели user

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4) 
-    tg_id = Column(BigInteger, unique=True, nullable=False)
+    tg_id = Column(BigInteger, unique=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     middle_name = Column(String, nullable=False)
