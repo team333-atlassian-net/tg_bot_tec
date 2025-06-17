e4510a931425cf30a888c86b1af3a8716864d983
vlkttlv <vitavitavita04@bk.ru>
2025-06-17 22:42:37 +0700

    добавила /help + роутеры users auth

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 from config import settings
 from dao.auth import get_user
-from handlers import canteen, auth
+from handlers import canteen, auth, users
 
 API_TOKEN = settings.API_TOKEN
 
@@ -28,13 +28,15 @@ async def start_handler(message: Message):
 @router.message(Command("help"))
 async def help_handler(message: Message):
     """Хэндлер со справочной информацией"""
+
     tg_id = message.from_user.id
     user = await get_user(tg_id=tg_id)
 
-    if not user:
+    if not user: # проверка что пользователь авторизован
         await message.answer("Вы не авторизованы. Введите пин-код с помощью /login.")
         return
-    if user.admin_rule:
+    
+    if user.admin_rule: # команды для админа
         help_text = (
             "📌 <b>Доступные команды для администратора:</b>\n\n"
             "/start — Начать взаимодействие с ботом\n"
@@ -43,7 +45,7 @@ async def help_handler(message: Message):
             "/add_user - Добавить пользователя(ей) в систему вручную или с помощью файла excel"
             "столовая — Информация о работе столовой\n"
         )
-    else:
+    else: # команды для обычного пользователя
         help_text = (
             "📌 <b>Доступные команды:</b>\n\n"
             "/start — Начать взаимодействие с ботом\n"
@@ -57,6 +59,7 @@ async def help_handler(message: Message):
 dp.include_router(router)
 dp.include_router(canteen.router)
 dp.include_router(auth.router)
+dp.include_router(users.router)
 
 async def main():
     await dp.start_polling(bot)
