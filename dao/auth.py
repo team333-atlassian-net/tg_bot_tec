d7c9b50a67eba70cf5f5a402e4c42a254044a90d
vlkttlv <vitavitavita04@bk.ru>
2025-06-18 22:18:29 +0700

    коммит

--- dao/auth.py
+++ dao/auth.py
@@ -1,11 +1,10 @@
 """Хэндлеры для работы с пользователями"""
 from sqlalchemy.future import select
-from models import User
+from models import User, RegistrationRequest
 from db import async_session_maker
 
-
 async def get_user(**filters):
-    """Функция для поиска по заданным критериям
+    """Функция для поиска пользователя по заданным критериям
 
     - Пример:
         user = await get_user(pin_code="1234qwe")
@@ -18,6 +17,20 @@ async def get_user(**filters):
         result = await session.execute(query)
         return result.scalar_one_or_none()
 
+async def get_users(**filters):
+    """Функция для поиска пользователей по заданным критериям
+
+    - Пример:
+        user = await get_users(pin_code="1234qwe")
+        user = await get_users(tg_id="1111111")
+    """
+    async with async_session_maker() as session:
+        query = select(User)
+        if filters:
+            query = query.filter_by(**filters)
+        result = await session.execute(query)
+        return result.scalars().all()
+    
 
 async def update_or_add_tg_id(user, tg_id):
     """Функция для обновления или добавления tg_id для пользователя"""
@@ -57,3 +70,42 @@ async def is_admin(tg_id: int) -> bool:
     """Функция, проверяющая, что роль - админ"""
     user = await get_user(tg_id=tg_id, admin_rule=True)
     return user is not None
+
+
+
+async def create_registration_request(tg_id: str, first_name: str, last_name: str, middle_name: str):
+    async with async_session_maker() as session:
+        request = RegistrationRequest(
+            tg_id=tg_id,
+            first_name=first_name,
+            last_name=last_name,
+            middle_name=middle_name,
+        )
+        session.add(request)
+        await session.commit()
+        return request
+    
+
+async def get_request(**filters):
+    async with async_session_maker() as session:
+        query = select(RegistrationRequest)
+        if filters:
+            query = query.filter_by(**filters)
+        result = await session.execute(query)
+        return result.scalar_one_or_none()
+
+async def get_pending_requests():
+    async with async_session_maker() as session:
+        result = await session.execute(
+            select(RegistrationRequest).where(RegistrationRequest.status == "pending")
+        )
+        return result.scalars().all()
+
+async def update_request_status(request_id, status):
+    async with async_session_maker() as session:
+        result = await session.execute(select(RegistrationRequest).where(RegistrationRequest.id == request_id))
+        req = result.scalar_one_or_none()
+        if req:
+            req.status = status
+            session.add(req)
+            await session.commit()
--- handlers/auth.py
+++ handlers/auth.py
@@ -1,9 +1,12 @@
 """Хэндлеры для работы с аутенфикацией пользователей"""
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from dao.auth import get_user, update_or_add_tg_id
+from aiogram import Bot, Router, F
+from dao.auth import add_user, create_registration_request, get_request, get_user, get_users, update_or_add_tg_id, update_request_status
 from aiogram.filters import Command
+from models import User
+from utils import generate_pin
 
 
 router = Router()
@@ -12,7 +15,14 @@ class AuthStates(StatesGroup):
     """Класс состояний для авторизации пользователя по ПИН-коду"""
     awaiting_pin = State()
 
+class RegisterRequestStates(StatesGroup):
+    """Класс состояний для регистрации пользователя"""
+    first_name = State()
+    last_name = State()
+    middle_name = State()
 
+#######################################################################
+# Авторизация пользователя
 @router.message(Command("login"))
 async def start_handler(message: Message, state: FSMContext):
     """Стартовый хэндлер для авторизации"""
@@ -29,14 +39,101 @@ async def handle_pin(message: Message, state: FSMContext):
     user = await get_user(pin_code=pin)
 
     if not user:
-        await message.answer("ПИН-код не найден.")
+        await message.answer("ПИН-код не найден. Попробуйте заново вызвать login")
+        await state.clear()
         return
 
     if user.tg_id and user.tg_id != tg_id:
-        await message.answer("Этот ПИН-код уже использован другим пользователем.")
+        await message.answer("Этот ПИН-код уже использован другим пользователем. Попробуйте заново вызвать login")
+        await state.clear()
         return
 
     await update_or_add_tg_id(user, tg_id)
 
     await message.answer(f"Добро пожаловать, {user.first_name}!")
     await state.clear()
+
+##########################################################
+# Регистрация пользователя
+
+@router.message(Command("register"))
+async def register_start(message: Message, state: FSMContext):
+    await state.set_state(RegisterRequestStates.first_name)
+    await message.answer("Введите ваше имя:")
+
+@router.message(RegisterRequestStates.first_name)
+async def enter_first_name(message: Message, state: FSMContext):
+    await state.update_data(first_name=message.text.strip())
+    await state.set_state(RegisterRequestStates.last_name)
+    await message.answer("Введите вашу фамилию:")
+
+@router.message(RegisterRequestStates.last_name)
+async def enter_last_name(message: Message, state: FSMContext):
+    await state.update_data(last_name=message.text.strip())
+    await state.set_state(RegisterRequestStates.middle_name)
+    await message.answer("Введите ваше отчество:")
+
+
+@router.message(RegisterRequestStates.middle_name)
+async def reg_middle_name(message: Message, state: FSMContext):
+    data = await state.get_data()
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    middle_name = message.text.strip()
+    tg_id = str(message.from_user.id)
+
+    request = await create_registration_request(tg_id, first_name, last_name, middle_name)
+    request_id = request.id
+
+    await message.answer("Заявка на регистрацию отправлена. Ожидайте подтверждения.")
+    await state.clear()
+
+    admins = await get_users(admin_rule=True)
+    for admin in admins:
+        kb = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="✅ Принять", callback_data=f"approve:{request_id}")],
+            [InlineKeyboardButton(text="❌ Отклонить", callback_data=f"reject:{request_id}")]
+        ])
+        await message.bot.send_message(
+            chat_id=admin.tg_id,
+            text=f"Новая заявка:\n<b>{last_name} {first_name} {middle_name}</b>",
+            reply_markup=kb
+        )
+
+
+@router.callback_query(F.data.startswith("approve:"))
+async def approve_request(callback: CallbackQuery):
+    request_id = callback.data.split(":")[1]
+    request = await get_request(id=request_id)
+    pin = generate_pin()
+
+    await update_request_status(request_id=request_id, status="approved")
+    user = User(
+        tg_id=int(request.tg_id),
+        first_name=request.first_name,
+        last_name=request.last_name,
+        middle_name=request.middle_name,
+        pin_code=pin,
+        admin_rule=False
+    )
+    await add_user(user)
+
+    await callback.message.bot.send_message(chat_id=int(request.tg_id), text=f"Ваша заявка одобрена. Ваш ПИН: <b>{pin}</b>")
+    await callback.message.answer(
+        text=f"👤 Пользователь <b>{request.last_name} {request.first_name}</b> успешно зарегистрирован.\n"
+             f"Ему отправлен ПИН-код: <b>{pin}</b>"
+    )
+
+    await callback.answer("Пользователь добавлен.")
+
+@router.callback_query(F.data.startswith("reject:"))
+async def reject_request(callback: CallbackQuery):
+    request_id = callback.data.split(":")[1]
+    request = await get_request(id=request_id)
+    await update_request_status(request_id=request_id, status="rejected")
+    await callback.message.bot.send_message(chat_id=int(request.tg_id), text="Ваша заявка отклонена.")
+    await callback.message.answer(
+        text=f"Заявка от <b>{request.last_name} {request.first_name}</b> была отклонена."
+    )
+
+    await callback.answer("Заявка отклонена.")
