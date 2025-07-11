8956e1e9c29552e39a4027d9af9532302858823a
vlkttlv <vitavitavita04@bk.ru>
2025-07-11 17:02:43 +0700

    оргструктура

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -7,7 +7,8 @@ from dialogs.add_user import add_user_dialog
 from dialogs.admin_events import admin_event_dialog
 from dialogs.manage_events import manage_event_dialog
 from dialogs.company_info import add_company_info_dialog
-from dialogs.organizational_structure import add_org_structure_dialog
+from dialogs.org_structure.add_org_structure import create_org_structure_dialog
+from dialogs.org_structure.view_org_structure import org_structure_dialog
 
 
 def register_all_dialogs(dp: Dispatcher):
@@ -18,5 +19,6 @@ def register_all_dialogs(dp: Dispatcher):
     dp.include_router(admin_event_dialog)
     dp.include_router(manage_event_dialog)
     dp.include_router(add_company_info_dialog)
-    dp.include_router(add_org_structure_dialog)
+    dp.include_router(create_org_structure_dialog)
+    dp.include_router(org_structure_dialog)
 
--- handlers/organizational_structure.py
+++ handlers/organizational_structure.py
@@ -1,42 +1,36 @@
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
-from dao.organizational_structure import get_all_organizational_structure
-from dialogs.organizational_structure import AddOrganizationalStructureSG
+from aiogram.types import Message
+
+from dialogs.org_structure.add_org_structure import OrgStructureCreationSG
+from dialogs.org_structure.view_org_structure import OrgStructureViewSG
 from utils.auth import require_admin, require_auth
 
 router = Router()
 
+@router.message(Command("org_structure"))
+async def show_virtual_excursions(message: Message, dialog_manager: DialogManager):
+    user = await require_auth(message)
+    if not user:
+        return
+    await dialog_manager.reset_stack()
+    await dialog_manager.start(OrgStructureViewSG.list, mode=StartMode.RESET_STACK)
+
+
 @router.message(Command("add_org_structure"))
-async def start_add_info(message: Message, dialog_manager: DialogManager):
+async def start_add_event(message, dialog_manager: DialogManager):
     user = await require_admin(message)
     if not user:
         return
     await dialog_manager.reset_stack()
-    await dialog_manager.start(AddOrganizationalStructureSG.title, mode=StartMode.RESET_STACK)
+    await dialog_manager.start(OrgStructureCreationSG.title, mode=StartMode.RESET_STACK)
 
-@router.message(Command("org_structure"))
-async def show_company_info(message: Message):
-    user = await require_auth(message)
-    if not user:
-        return
-    infos = await get_all_organizational_structure()
-    if not infos:
-        await message.answer("ℹ️ Пока нет информации об организационной структуре компании.")
-        return
 
-    for info in infos:
-        text = f"🏢 <b>{info.title}</b>"
-        if info.content:
-            text += f"\n\n{info.content}"
-
-        if info.file_id:
-            await message.bot.send_document(
-                chat_id=message.chat.id,
-                document=info.file_id,
-                caption=text,
-                parse_mode="HTML",
-            )
-        else:
-            await message.answer(text, parse_mode="HTML")
+# @router.message(Command("manage_org_structure"))
+# async def start_manage_events(message, dialog_manager: DialogManager):
+#     user = await require_admin(message)
+#     if not user:
+#         return
+#     await dialog_manager.reset_stack()
+#     await dialog_manager.start(ManageEventSG.list, mode=StartMode.RESET_STACK)
