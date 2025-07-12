397c5049cd589b2c7be2d2d0298dbf241c91be93
vlkttlv <vitavitavita04@bk.ru>
2025-07-12 16:26:26 +0700

    переделала company_info под общий ui

--- dao/company_info.py
+++ dao/company_info.py
@@ -17,8 +17,39 @@ async def add_company_info(title: str,
         await session.commit()
 
 
-
 async def get_all_company_info():
     async with async_session_maker() as session:
         result = await session.execute(select(CompanyInfo))
         return result.scalars().all()
+    
+
+async def get_company_info_by_id(company_info_id: int):
+    async with async_session_maker() as session:
+        res = await session.execute(select(CompanyInfo).where(CompanyInfo.id == company_info_id))
+        return res.scalar_one_or_none()
+
+
+async def update_company_info(company_info_id: int,
+                               new_title: str = None,
+                               new_description: str = None,
+                               file_id: str = None,
+                               image_id: str = None):
+    async with async_session_maker() as session:
+        company_info = await session.get(CompanyInfo, company_info_id)
+        if company_info:
+            if new_title is not None:
+                company_info.title = new_title
+            if new_description is not None:
+                company_info.content = new_description
+            if file_id is not None:
+                company_info.file_path = file_id
+            if image_id is not None:
+                company_info.image_path = image_id
+            await session.commit()
+
+async def delete_company_info(company_info_id: str):
+    async with async_session_maker() as session:
+        company_info = await session.get(CompanyInfo, company_info_id)
+        if company_info:
+            await session.delete(company_info)
+            await session.commit()
--- handlers/company_info.py
+++ handlers/company_info.py
@@ -1,57 +1,42 @@
+import logging
+
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
-from dao.company_info import get_all_company_info
-from dialogs.company_info import AddCompanyInfoSG
+from aiogram.types import Message
+
+from dialogs.company_info.add_company_info import CompanyInfoCreationSG
+from dialogs.company_info.manage_view_company import ManageCompanyInfoSG
+from dialogs.company_info.view_company_info import CompanyInfoViewSG
 from utils.auth import require_admin, require_auth
+
+logger = logging.getLogger(__name__)
 
 router = Router()
 
+@router.message(Command("company_info"))
+async def show_virtual_excursions(message: Message, dialog_manager: DialogManager):
+    user = await require_auth(message)
+    if not user:
+        return
+    await dialog_manager.reset_stack()
+    logger.info("Пользователь запросил информацию о компании (/company_info)")
+    await dialog_manager.start(CompanyInfoViewSG.list, mode=StartMode.RESET_STACK)
+
+
 @router.message(Command("add_company_info"))
-async def start_add_info(message: Message, dialog_manager: DialogManager):
+async def start_add_company_info(message, dialog_manager: DialogManager):
     user = await require_admin(message)
     if not user:
         return
     await dialog_manager.reset_stack()
-    await dialog_manager.start(AddCompanyInfoSG.title, mode=StartMode.RESET_STACK)
+    await dialog_manager.start(CompanyInfoCreationSG.title, mode=StartMode.RESET_STACK)
 
-@router.message(Command("company_info"))
-async def show_company_info(message: Message):
-    user = await require_auth(message)
+
+@router.message(Command("manage_company_info"))
+async def start_manage_events(message, dialog_manager: DialogManager):
+    user = await require_admin(message)
     if not user:
         return
-    infos = await get_all_company_info()
-    if not infos:
-        await message.answer("ℹ️ Пока нет информации о компании.")
-        return
-
-    for info in infos:
-        text = f"🏢 <b>{info.title}</b>"
-        if info.content:
-            text += f"\n\n{info.content}"
-
-        # если есть изображение — отправляем его с подписью
-        if info.image_path:
-            await message.bot.send_photo(
-                chat_id=message.chat.id,
-                photo=info.image_path,
-                caption=text,
-                parse_mode="HTML",
-            )
-            # Отдельно добавляем файл, если есть
-            if info.file_path:
-                await message.bot.send_document(
-                    chat_id=message.chat.id,
-                    document=info.file_path
-                )
-        elif info.file_path:
-            await message.bot.send_document(
-                chat_id=message.chat.id,
-                document=info.file_path,
-                caption=text,
-                parse_mode="HTML",
-            )
-        else:
-            # Ни изображения, ни файла — просто текст
-            await message.answer(text, parse_mode="HTML")
+    await dialog_manager.reset_stack()
+    await dialog_manager.start(ManageCompanyInfoSG.list, mode=StartMode.RESET_STACK)
