d1590884a1a908434333f33644153d9d0102a838
vlkttlv <vitavitavita04@bk.ru>
2025-07-29 04:17:44 +0700

    выход из аккаунта доделан

--- dialogs/auth/logout/handlers.py
+++ dialogs/auth/logout/handlers.py
@@ -8,7 +8,7 @@ async def on_logout_click(
     callback: CallbackQuery, widget, dialog_manager: DialogManager
 ):
     """Хэндлер выхода из аккаунта. Очищает tg_id у пользователя"""
-    tg_id = callback.message.from_user.id
+    tg_id = callback.from_user.id
     await delete_tg_id(tg_id)
-    await callback.answer("Вы вышли из аккаунта")
+    await callback.message.answer("Вы вышли из аккаунта")
     await dialog_manager.done()
